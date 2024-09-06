d9910030b495c45ccc71366d02cd56d9c1bdf16b
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-06 17:33:08 -0500

    Exercice

--- 03-primeros-pasos/03-component/src/main.jsx
+++ 03-primeros-pasos/03-component/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 //IMPORTO LA FUNCION
 import { App , FirstApp} from "./HelloWorldApp";
-
+import{counterApp} from "./CounterApp"
 //LLAMAMOS AL CSS QUE SE APLICARA A TODO LO QUE EL MAIN RENDERICE
 import './styles.css';
 
@@ -19,5 +19,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <FirstApp title = "props soy un props" /> {/* LLAMADA A LA FUNCION APP Y PASAMOS VALOR DE ATRIBUTOS*/}
+    </React.StrictMode>
+)
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <counterApp value = {123} /> {/* LLAMADA A LA FUNCION APP Y PASAMOS VALOR DE ATRIBUTOS*/}
     </React.StrictMode>
 )
