6acb4f679789bde621c7600e6bc6f4acd49ca54d
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-06 17:49:08 -0500

    clickEvent

--- 03-primeros-pasos/03-component/src/main.jsx
+++ 03-primeros-pasos/03-component/src/main.jsx
@@ -2,29 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 //IMPORTO LA FUNCION
 import { App , FirstApp} from "./HelloWorldApp";
-import{counterApp} from "./CounterApp"
+import {CounterApp} from "./CounterApp"
 //LLAMAMOS AL CSS QUE SE APLICARA A TODO LO QUE EL MAIN RENDERICE
 import './styles.css';
 
 
 //RENDERIZAMOS EN UN ELEMENTO CON ID ROOT
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App /> {/* LLAMADA A LA FUNCION APP */}
-    </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <React.StrictMode>
+//         <App /> {/* LLAMADA A LA FUNCION APP */}
+//     </React.StrictMode>
+// )
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <React.StrictMode>
+//         <FirstApp title = "props soy un props" /> {/* LLAMADA A LA FUNCION APP Y PASAMOS VALOR DE ATRIBUTOS*/}
+//     </React.StrictMode>
+// )
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <FirstApp title = "props soy un props" /> {/* LLAMADA A LA FUNCION APP Y PASAMOS VALOR DE ATRIBUTOS*/}
-    </React.StrictMode>
-)
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <counterApp value = {123} /> {/* LLAMADA A LA FUNCION APP Y PASAMOS VALOR DE ATRIBUTOS*/}
+        <CounterApp value = {123} /> {/* LLAMADA A LA FUNCION APP Y PASAMOS VALOR DE ATRIBUTOS*/}
     </React.StrictMode>
 )
