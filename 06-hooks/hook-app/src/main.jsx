4be2d50f04e7a79a59918deb3f9dcf203eab0ef6
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-11 12:03:50 -0500

    useLayoutEffect

--- 06-hooks/hook-app/src/main.jsx
+++ 06-hooks/hook-app/src/main.jsx
@@ -8,9 +8,10 @@ import { SimpleForm } from './02-useEffect/SimpleForm.jsx'
 import { FormWithCustom } from './02-useEffect/FormWithCustom.jsx'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx'
 import { FocusScreen } from './04-useRef/FocusScreen.jsx'
+import { Layout } from './05-useLayoutEffect/Layout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FocusScreen/>
+    <Layout/>
   </StrictMode>,
 )
