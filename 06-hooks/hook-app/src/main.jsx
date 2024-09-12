74cd3257aec5ad9176a9fd56d6d57cb5a108a913
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-11 21:21:08 -0500

    entorno

--- 06-hooks/hook-app/src/main.jsx
+++ 06-hooks/hook-app/src/main.jsx
@@ -9,9 +9,13 @@ import { FormWithCustom } from './02-useEffect/FormWithCustom.jsx'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx'
 import { FocusScreen } from './04-useRef/FocusScreen.jsx'
 import { Layout } from './05-useLayoutEffect/Layout.jsx'
+import { Memorize } from './06-MEMOS/Memorize.jsx'
+import { MemoHook } from './06-MEMOS/memoHook.jsx'
+import { CallBackHook } from './06-MEMOS/CallBackHook.jsx'
+import { Padre } from './07-tarea-memo/Padre.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Layout/>
+    <Padre/>
   </StrictMode>,
 )
